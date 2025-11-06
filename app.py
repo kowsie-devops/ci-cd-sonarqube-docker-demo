d1fd741bd300481be25c205e52e7965ec2233db4
kowsie <youremail@example.com>
2025-11-06 13:47:15 +0000

    Modified

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def message():
-    return "Hello, CI/CD pipeline with SonarQube and Docker!"
+    return "Hello!, CI/CD pipeline with SonarQube and Docker!"
 
 if __name__ == "__main__":
     print(message())
