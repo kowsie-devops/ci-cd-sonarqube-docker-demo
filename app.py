3bfcf45cfb6c62bb9be700a278d2b619b6816b71
kowsie <youremail@example.com>
2025-11-06 04:42:33 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,3 @@
+# app.py
+print("Hello, CI/CD Pipeline with Jenkins, SonarQube, and Docker!")
+
---
+++ test_app.py
@@ -0,0 +1,4 @@
+# test_app.py
+def test_message():
+    assert "CI/CD" in "Hello, CI/CD Pipeline with Jenkins, SonarQube, and Docker!"
+
